9eecbf164f1bd0d5cba8c6ea3aba70b3147db47a
SuccEgg <rrudolfs@inbox.lv>
2022-09-30 11:43:19 +0300

    Class_

---
+++ Class_06/pirm.c
@@ -0,0 +1,21 @@
+// komentāra viena rinda- // tiek apzīmēts tikai komentāra sākums
+//komentāra cita rinda
+
+/*
+komentāra paragrāfs
+izmanto, piemēram, lai komentētu koda fragmentus
+*/
+
+int main ()
+{
+//if()
+//{
+//
+//}
+
+return 0;
+
+
+//return 100
+}
+
