db1022b0af3a1bfc4cc29e0933de026d079f5988
SuccEgg <113163008+SuccEgg@users.noreply.github.com>
2023-01-20 22:07:42 +0200

    Add files via upload

---
+++ LD1/1LD_series.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+void main(){
+ double x,y,a,S;
+ int k=0;
+ y=sin(x/2);
+printf("y=sin(%.2f/2)=%.2f\n",x,y);
+
+ a=pow(-1,k)*pow(x,2*k+1)/(1.*pow(2,2*k+1));
+ S=a;
+
